203303d2a55bbf5c61c7b9a712d756577a364427
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-11-03 11:21:58 -0400

    added error handling for empty lists

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -40,80 +40,92 @@ public class Dealership {
         this.phone = phone;
     }
 
-    public ArrayList<Vehicle> getInventory() {
-        return inventory;
-    }
-
-    public void setInventory(ArrayList<Vehicle> inventory) {
-        this.inventory = inventory;
-    }
-
-    @Override
-    public String toString() {
-        return "Dealership{" +
-                "name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", phone='" + phone + '\'' +
-                ", inventory=" + inventory +
-                '}';
-    }
-
     public List<Vehicle> getVehiclesByPrice(double min, double max) {
         List<Vehicle> vehicles = new ArrayList<>();
+        boolean found = false;
         for(Vehicle v: inventory){
             if(v.getPrice() >= min && v.getPrice() <= max){
                 vehicles.add(v);
+                found = true;
             }
+        }
+        if (!found){
+            System.out.println("no results.");
         }
         return vehicles;
     }
 
     public List<Vehicle> getVehiclesByMakeModel(String make, String model){
         List<Vehicle> vehicles = new ArrayList<>();
+        boolean found = false;
         for(Vehicle v: inventory){
             if(v.getMake().equalsIgnoreCase(make) && v.getModel().equalsIgnoreCase(model)){
                 vehicles.add(v);
+                found = true;
             }
+        }
+        if (!found){
+            System.out.println("no results.");
         }
         return vehicles;
     }
 
     public List<Vehicle> getVehiclesByYear(int min, int max) {
         List<Vehicle> vehicles = new ArrayList<>();
+        boolean found = false;
         for(Vehicle v: inventory){
             if(v.getYear() >= min && v.getYear() <= max){
                 vehicles.add(v);
+                found = true;
             }
+        }
+        if (!found){
+            System.out.println("no results.");
         }
         return vehicles;
     }
 
     public List<Vehicle> getVehiclesByColor(String color){
         List<Vehicle> vehicles = new ArrayList<>();
+        boolean found = false;
         for(Vehicle v: inventory){
             if(v.getColor().equalsIgnoreCase(color)){
                 vehicles.add(v);
+                found = true;
             }
+        }
+        if (!found){
+            System.out.println("no results.");
         }
         return vehicles;
     }
 
     public List<Vehicle> getVehiclesByMileage(int odometer){
         List<Vehicle> vehicles = new ArrayList<>();
+        boolean found = false;
         for(Vehicle v: inventory){
             if(v.getOdometer() == odometer){
                 vehicles.add(v);
+                found = true;
             }
+        }
+        if (!found){
+            System.out.println("no results.");
         }
         return vehicles;
     }
 
     public List<Vehicle> getVehiclesByType(String vehicleType){
         List<Vehicle> vehicles = new ArrayList<>();
+        boolean found = false;
         for(Vehicle v: inventory){
             if(v.getVehicleType().equalsIgnoreCase(vehicleType)){
                 vehicles.add(v);
+                found = true;
             }
+        }
+        if (!found){
+            System.out.println("no results.");
         }
         return vehicles;
     }
