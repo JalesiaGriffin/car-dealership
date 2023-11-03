a3050592779ecca5c530512acfb7557bffe01fe7
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-11-02 20:10:07 -0400

    created methods

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,105 @@
 package com.pluralsight;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class UserInterface {
+
+    private Dealership dealership;
+
+    public UserInterface() {
+        this.dealership = init();
+    }
+
+
+    public void display(){
+    Scanner scan = new Scanner(System.in);
+    init();
+
+    boolean running = true;
+    while (running) {
+        menu();
+        System.out.print("Select an option: ");
+        int option = scan.nextInt();
+        scan.nextLine();
+
+        switch (option) {
+            case 1 -> processGetByPriceRequest();
+            case 2 -> processGetByMakeModelRequest();
+            case 3 -> processGetByYearRequest();
+            case 4 -> processGetByColorRequest();
+            case 5 -> processGetByMileageRequest();
+            case 6 -> processGetByVehicleTypeRequest();
+            case 7 -> processGetAllVehiclesRequest();
+            case 8 -> processAddVehicleRequest();
+            case 9 -> processRemoveVehicleRequest();
+            case 99 -> running = false;
+            default -> System.out.println("invalid response.");
+        }
+    }
+
+
+    }
+
+    public void processGetByPriceRequest(){
+
+    }
+
+    public void processGetByMakeModelRequest(){
+
+    }
+
+    public void processGetByYearRequest(){
+
+    }
+
+    public void processGetByColorRequest(){
+
+    }
+
+    public void processGetByMileageRequest(){
+
+    }
+
+    public void processGetByVehicleTypeRequest(){
+
+    }
+
+    public void processGetAllVehiclesRequest(){
+        List<Vehicle> vehicles = dealership.getAllVehicles();
+        displayVehicles(vehicles);
+    }
+
+    public void processAddVehicleRequest(){
+
+    }
+
+    public void processRemoveVehicleRequest(){
+
+    }
+
+    public void menu() {
+        System.out.println("Menu");
+        System.out.println("1 - Find vehicles within a price range");
+        System.out.println("2 - Find vehicles by make / model");
+        System.out.println("3 - Find vehicles by year range");
+        System.out.println("4 - Find vehicles by color");
+        System.out.println("5 - Find vehicles by mileage range");
+        System.out.println("6 - Find vehicles by type (car, truck, SUV, van)");
+        System.out.println("7 - List ALL vehicles");
+        System.out.println("8 - Add a vehicle");
+        System.out.println("9 - Remove a vehicle");
+        System.out.println("99 - Quit\n");
+    }
+
+    private void displayVehicles(List<Vehicle> vehicles) {
+        for(Vehicle v: vehicles){
+            System.out.println(v);
+        }
+    }
+
+    private Dealership init(){
+        DealershipFileManager dfm = new DealershipFileManager();
+        return dfm.getDealership();
+    }
 }
